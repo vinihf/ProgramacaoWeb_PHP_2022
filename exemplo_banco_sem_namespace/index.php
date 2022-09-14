9891126416ce8735fef4d94e41c19cb7c3b0c530
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2022-09-14 07:27:57 -0300

    sem namespace

---
+++ exemplo_banco_sem_namespace/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__."/vendor/autoload.php";
+
+//Inserindo novo registro no banco
+$pessoa = new Pessoa("Primeira usuária","meuemail@email.com");
+$pessoa->save();
+
+//Atualizando registro no banco
+$p = Pessoa::find(7);
+$p->setEmail("troquei-email@email.com");
+$p->save();
+
+//Pesquisando um registro
+$p2 = Pessoa::find(8);
+echo $p2->getNome();
+
+//Deletando um registro
+$p2->delete();
